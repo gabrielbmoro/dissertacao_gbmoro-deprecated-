dc80d28a66c475f2ecb793fbfdec2f5f64db1a6a
Gabriel B Moro <tido4410@gmail.com>
2016-07-09 17:56:01 -0300

    Some codes of Matrices Multiplication for tests

---
+++ benchmarks/MM/.opari2_init.c
@@ -0,0 +1,33 @@
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+#include <stddef.h>
+
+extern void POMP2_Init_reg_ane4xg11a972e_1();
+
+void POMP2_Init_regions()
+{
+    POMP2_Init_reg_ane4xg11a972e_1();
+}
+
+size_t POMP2_Get_num_regions()
+{
+    return 1;
+}
+
+
+void POMP2_USER_Init_regions()
+{
+}
+
+size_t POMP2_USER_Get_num_regions()
+{
+    return 0;
+}
+
+#ifdef __cplusplus
+}
+#endif
